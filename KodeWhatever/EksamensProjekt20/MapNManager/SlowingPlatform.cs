92bfaac9d4f4f4ffe58166c2a9013be20f7ca03a
Jokkefu <joak0732@student.eadania.dk>
2020-05-22 17:45:52 +0200

    Platforme opsatte

--- KodeWhatever/EksamensProjekt20/MapNManager/SlowingPlatform.cs
+++ KodeWhatever/EksamensProjekt20/MapNManager/SlowingPlatform.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace EksamensProjekt20.MapNManager
 {
     class SlowingPlatform : TerrainPiece
     {
+        public SlowingPlatform(Vector2 pos, Vector2 size)
+        {
+            screenPosition = pos;
+            spriteSize = size;
+            sprite = Game1.contMan.Load<Texture2D>("SlowingPlatform");
+        }
     }
 }
--- KodeWhatever/EksamensProjekt20/MapNManager/StageBlock.cs
+++ KodeWhatever/EksamensProjekt20/MapNManager/StageBlock.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace EksamensProjekt20.MapNManager
 {
     class StageBlock
     {
-        public List<TerrainPiece> terrainSetup = new List<TerrainPiece>;
+        public List<TerrainPiece> terrainSetup = new List<TerrainPiece>();
+        public StageBlock(int stageID)
+        {
+            switch (stageID)
+            {
+                case 1:
+                    terrainSetup.Add(new NormalPlatform(new Vector2(194, 247), new Vector2(382, 459)));
+                    terrainSetup.Add(new SlowingPlatform(new Vector2(301, 151), new Vector2(538, 329)));
+                    terrainSetup.Add(new FallingPlatform(new Vector2(568, 120), new Vector2(716, 227)));
+                    terrainSetup.Add(new TriggerPlatform(new Vector2(116, 492), new Vector2(390, 617)));
+                    terrainSetup.Add(new TriggerPlatform(new Vector2(468, 399), new Vector2(894, 509)));
+                    terrainSetup.Add(new TriggerPlatform(new Vector2(132, 656), new Vector2(482, 736)));
+                    terrainSetup.Add(new TriggerPlatform(new Vector2(148, 762), new Vector2(208, 956)));
+                    break;
 
+            }
+        }
     }
 }
