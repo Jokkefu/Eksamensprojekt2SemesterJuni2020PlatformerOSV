fe8686bf161ace345e277633ac3c3838f625c3a4
Loushie <57252834+Loushie@users.noreply.github.com>
2020-05-25 20:03:29 +0200

    kjldsa

--- KodeWhatever/EksamensProjekt20/CommandPattern/InputHandler.cs
+++ KodeWhatever/EksamensProjekt20/CommandPattern/InputHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EksamensProjekt20.Characters;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,30 @@ namespace EksamensProjekt20.CommandPattern
 {
     class InputHandler
     {
+        private Dictionary<Keys, ICommand> keybinds = new Dictionary<Keys, ICommand>();
 
+        public InputHandler()
+        {
+            keybinds.Add(Keys.D, new MoveCommand(new Vector2(1, 0)));
+            keybinds.Add(Keys.A, new MoveCommand(new Vector2(-1, 0)));
+            keybinds.Add(Keys.W, new JumpCommand)
+            keybinds.Add(Keys.J, new PrimaryAttackCommand)
+            keybinds.Add(Keys.K, new SecondaryAttackCommand)
+
+        }
+
+        public void Excecute(Player player)
+        {
+            KeyboardState keyState = Keyboard.GetState();
+
+            foreach (Keys key in keybinds.Keys)
+            {
+                if (keyState.IsKeyDown(key))
+                {
+                    keybinds[key].Execute(player);
+                }
+            }
+
+        }
     }
 }
