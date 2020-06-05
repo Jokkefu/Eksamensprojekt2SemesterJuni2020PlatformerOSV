d20972ee3341eeab30725b55e6af3be71fd495c9
DanielJVestergaard <reignofheroes@gmail.com>
2020-06-05 18:56:14 +0200

    Menu Polish

--- KodeWhatever/EksamensProjekt20/State/MenuState.cs
+++ KodeWhatever/EksamensProjekt20/State/MenuState.cs
@@ -21,7 +21,7 @@ namespace EksamensProjekt20.States
 
             var newGameButton = new Button(buttonTexture, buttonFont)
             {
-                Position = new Vector2(300, 200),
+                Position = new Vector2(700, 150),
                 Text = "New Game",
             };
 
@@ -29,7 +29,7 @@ namespace EksamensProjekt20.States
 
             var LoadGameButton = new Button(buttonTexture, buttonFont)
             {
-                Position = new Vector2(300, 250),
+                Position = new Vector2(700, 400),
                 Text = "Load Game",
             };
 
@@ -37,7 +37,7 @@ namespace EksamensProjekt20.States
 
             var QuitGameButton = new Button(buttonTexture, buttonFont)
             {
-                Position = new Vector2(300, 300),
+                Position = new Vector2(700, 650),
                 Text = "Quit Game",
             };
 
@@ -70,12 +70,8 @@ namespace EksamensProjekt20.States
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.Begin();
-
             foreach (var component in components)
                 component.Draw(gameTime, spriteBatch);
-
-            spriteBatch.End();
         }
         public override void PostUpdate(GameTime gameTime)
         {
