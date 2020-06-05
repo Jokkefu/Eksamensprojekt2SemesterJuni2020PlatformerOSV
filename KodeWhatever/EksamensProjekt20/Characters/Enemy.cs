8b24a1a568d358fa914bef9edf49c9e0423f689c
DanielJVestergaard <reignofheroes@gmail.com>
2020-06-05 18:50:46 +0200

    Sidste til Menuen, samt det der manglede til Enemy for at StatePattern virker. Samt hvad der manglede til Enemy AI - StatePattern

--- KodeWhatever/EksamensProjekt20/Characters/Enemy.cs
+++ KodeWhatever/EksamensProjekt20/Characters/Enemy.cs
@@ -4,15 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EksamensProjekt20.StatePattern;
 
 namespace EksamensProjekt20.Characters
 {
     class Enemy : Character
     {
+        private IEnemyState currentState;
         public Enemy(Vector2 position)
         {
             screenPosition = position;
             spriteSize = new Vector2(20, 50);
+            ChangeState(new Idle());
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            currentState.Execute();
+        }
+        public void ChangeState(IEnemyState newEnemyState)
+        {
+            if (currentState != null)
+            {
+                currentState.Exit();
+            }
+
+            currentState.Enter(this);
         }
     }
 }
---
+++ KodeWhatever/EksamensProjekt20/StatePattern/Idle.cs
@@ -0,0 +1,47 @@
+﻿using EksamensProjekt20.Characters;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensProjekt20.StatePattern
+{
+    class Idle : IEnemyState
+    {
+        private Vector2 velocity;
+
+        private float cooldown;
+
+        private float elapsed;
+
+        private Enemy enemy;
+
+
+        public void Enter(Enemy enemy)
+        {
+            this.enemy = enemy;
+            enemy.movementSpeed = 50;
+            elapsed = 0;
+            cooldown = 3;
+
+        }
+
+        public void Execute()
+        {
+            //ChangeState
+            elapsed += (float)Game1.gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (elapsed >= cooldown)
+            {
+                enemy.ChangeState(new Attack());
+            }
+        }
+
+        public void Exit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
