8707c37f22f6949692b3206bbd029eeec7ac63c1
Rasmus0008 <Powermine@jubiigames.dk>
2020-06-06 15:00:55 +0200

    Enemy collision

--- KodeWhatever/EksamensProjekt20/GameObject.cs
+++ KodeWhatever/EksamensProjekt20/GameObject.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EksamensProjekt20.Characters;
+using EksamensProjekt20.Buffs;
+using System.Runtime.CompilerServices;
 
 namespace EksamensProjekt20
 {
@@ -19,9 +22,12 @@ namespace EksamensProjekt20
 
         
 
-        public void CollisionDetection(GameObject other)
+        public void GroundCollisionDetection(GameObject other)
         {
             TerrainPiece t = (TerrainPiece)other;
+
+            //Har hvad vi kollidere med Tag Terrain
+            //FIX COLLISION CALCULATIONS t.spriteRect.Top/Bottom/Left/Right
             if (other.tag == "Terrain")
             {
                 //Bottom Player Collision
@@ -50,8 +56,26 @@ namespace EksamensProjekt20
                     int width = spriteRect.Left - other.spriteRect.Right;
                     gamePosition.X -= width;
                 }
-
             }
+        }
+        public void CharacterCollisionDetection(Character other1)
+        {
+            Character c = (Character)other1;
+            if (other1.currentHealth >= 1)
+            {
+                // Y- = OP
+                // Y+ = NED
+                // X- = Venstre
+                // X+ = Højre
+                //c er den charecter vi kollidere med
+
+
+                if (/*TOP*/c.spriteRect.Top >= this.spriteRect.Top &&/*BOTTOM*/ c.spriteRect.Bottom <= this.spriteRect.Bottom &&/*RIGHT*/ c.spriteRect.Right <= this.spriteRect.Right &&/*LEFT*/ c.spriteRect.Left >= this.spriteRect.Left) 
+                {
+                    c.AddBuff(new Burn(/*DMG*/10,/*DURATION SEC*/10,/*TICKCOUNT*/10,/*THE TARGET*/c));
+                }
+            }
+
         }
         private void Jump()
         {
